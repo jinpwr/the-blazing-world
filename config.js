f3932d240895881027f4f0a4b7836a567b3b8b41
jinpwr <jayannpark@gmail.com>
2026-08-18 12:35:43 +0900

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-/* The only file you need to edit after forking.
-   owner/repo/branch are used by the editor at /admin.html to save posts. */
-window.SITE = {
-  title:  "The Blazing World",
-  tagline: "Notes from projects in progress",
-  owner:  "jinpwr",
-  repo:   "the-blazing-world",
-  branch: "main"
-};
